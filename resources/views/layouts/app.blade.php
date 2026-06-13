d13842991ea465129940df68c955f0746509b026
angieio <angieluz.vasquez@tecsup.edu.pe>
2026-06-13 13:06:07 -0500

    hagasn git

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -250,7 +250,7 @@
             <div class="flex flex-col gap-3.5 text-sm text-[oklch(0.91 0.12 95.75)]/70 font-sans">
                 <div class="flex items-center gap-3">
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
-                    <span>+51 934 008 523</span>
+                    <span>+51 907 210 407</span>
                 </div>
             </div>
         </div>
