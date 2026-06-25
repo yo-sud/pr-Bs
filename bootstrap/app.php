658ee6f2224eaae189178eb2f772192d42133abc
Yo <yamil.ochoa@tecsup.edu.pe>
2026-06-25 17:22:45 -0500

    CHI QUITE EL TOKEN

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         
         $middleware->validateCsrfTokens(except: [
-            'webhooks/pagos/falso',
+            '*',
         ]);
 
         $middleware->redirectTo(
