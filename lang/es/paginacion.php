86b4863243c7244fe2a34ec44ce812bdb5e777e0
Alexis Prieto <alexisprietohuiza0826@gmail.com>
2026-06-27 12:39:04 -0500

    sssss

--- lang/es/paginacion.php
+++ lang/es/paginacion.php
@@ -1,12 +1,6 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Componente de Paginación en Español
-    |--------------------------------------------------------------------------
-    */
-
     'previous' => '&laquo; Anterior',
     'next'     => 'Siguiente &raquo;',
     'showing'  => 'Mostrando',
